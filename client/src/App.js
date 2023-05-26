b7a2c617afec01e3e29bb7535492219291bb2ded
[josiah stelzl] <josiahstelzl@gmail.com>
2023-05-26 12:34:24 -0400

    added basic components, configured middleware

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+import NavLink from "./components/NavLink";
+import Footer from "./components/Footer";
+import Login from "./components/Login";
+import MyPage from "./components/MyPage";
+import GroupPage from "./components/GroupPage";
+// import DataPage from "./components/DataPage";
+import ManageGroupsPage from "./components/ManageGroupsPage";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <NavLink />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/my-tasks" element={<MyPage />} />
+          <Route path="/group/:id" element={<GroupPage />} />
+          <Route path="/manage-groups" element={<ManageGroupsPage />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </ApolloProvider>
   );
 }
 
