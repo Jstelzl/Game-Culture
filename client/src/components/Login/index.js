286c5f51f01d4f29b48246d8573a4a40bbff5918
[josiah stelzl] <josiahstelzl@gmail.com>
2023-05-26 12:52:33 -0400

    changed color of last login button for test

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -225,7 +225,7 @@ function Login() {
                     </div>
                     <div>
                         <button
-                            className="bg-blue-900 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded-full flex flex-row mx-auto"
+                            className="bg-green-900 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded-full flex flex-row mx-auto"
                             outline={true}
                         >
                             Sign Up
